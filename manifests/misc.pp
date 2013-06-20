9253437921b9d7764497a8fa80c052522e794984
Alexander Bien <abien@gmx.net>
2013-06-20 10:09:29 +0200

    wrong (old) variable name

--- manifests/misc.pp
+++ manifests/misc.pp
@@ -1,14 +1,9 @@
 class bootstrap::misc {
 
-  if ! ${bootstrap::root_email} {
-    fail("Module ${module_name} is missing \${bootstrap::root_email}. Please set!")
-  }
-
-
   mailalias { 'local root phone home':
     ensure    => present,
     name      => 'root',
-    recipient => '$bootstrap_misc_admin_email',
+    recipient => '$root_email',
     notify    => Exec['/usr/bin/newaliases']
   }
 
