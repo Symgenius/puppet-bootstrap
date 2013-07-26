a682dfc47611a4f2b83c85b47645091bc62fde50
Alexander Bien <abien@gmx.net>
2013-07-26 14:07:04 +0200

    fixes nothing really

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,25 +6,16 @@
 #
 # Document parameters here.
 #
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
+# [*root_email*]
+#   Where to send root emails to? ex cron outputs
+#   Defaults to root@localhost
 #
 # === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if it
-#   has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should not be used in preference to class parameters  as of
-#   Puppet 2.6.)
 #
 # === Examples
 #
 #  class { bootstrap:
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ]
+#    root_email => 'you@yourhost.com'
 #  }
 #
 # === Authors
